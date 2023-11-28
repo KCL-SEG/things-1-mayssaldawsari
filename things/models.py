16bb99f45ecedc5c598adab87401193ee5668913
Mayss Aldawsari <mayss.aldawsari@gmail.com>
2023-11-28 23:33:25 +0000

    Modify description

--- things/models.py
+++ things/models.py
@@ -3,7 +3,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Thing(models.Model):
     name = models.CharField(max_length=30, unique=True, blank=False)
-    description = models.TextField(max_length=120, blank=True)
+    description = models.CharField(max_length=120, blank=True)
     quantity = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
 
     def __str__(self):
